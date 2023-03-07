8d9e4ec2f54111d8d6ec203683311dc5a7d1eace
nick hannay <nhannay@sfu.ca>
2023-03-07 15:34:47 -0800

    bluetooth module can now scan for available devices

--- source-files/bluetooth.c
+++ source-files/bluetooth.c
@@ -3,6 +3,19 @@
 #include <stdbool.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <errno.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+
+// max number of devices that can respond to a bluetooth scan query
+#define MAX_DEV_RSP 255 
+
+// the duration of the bluetooth scan = 1.28s * SCAN_LENGTH
+#define SCAN_LENGTH 8
 
 
 // Bluetooth Module Threading
@@ -11,24 +24,107 @@ static bool stopping = false;
 static pthread_t bluetooth_thread_id;
 pthread_mutex_t mtx_bluetooth = PTHREAD_MUTEX_INITIALIZER;
 
+// Bluetooth Info
+int bt_adapter_id;
+int bt_sock_fd;
+
+/*typedef struct{
+    int num_devices;
+    inquiry_info* devices;
+}scanned_devices;*/
+
+
+
+
 
 void Bluetooth_init(void){
-
+    // get default bt adapter
+    bt_adapter_id = hci_get_route(NULL);
+    checkError();
+    bt_sock_fd = hci_open_dev(bt_adapter_id);
+    checkError();
     pthread_create(&bluetooth_thread_id, NULL, bluetoothThread, NULL);
+}
+
+
+void Bluetooth_displayAvailableDevices(inquiry_info* devices, int num_devices){
+    char addr[19] = { 0 };
+    char name[256] = { 0 };
+    for (int i = 0; i < num_devices; i++) {
+        ba2str(&(devices+i)->bdaddr, addr);
+        memset(name, 0, sizeof(name));
+        if (hci_read_remote_name(bt_sock_fd, &(devices+i)->bdaddr, sizeof(name), name, 0) < 0){
+            strcpy(name, "[unknown]");
+        }
+        printf("%s  %s\n", addr, name);
+    }
 
 }
 
 
 
+
 void* bluetoothThread(void* args){
     while(!stopping){
-
-        printf("hello\n");
-
-
+        printf("Scanning for bluetooth devices...\n");
+        inquiry_info* scanned_devices = malloc(MAX_DEV_RSP * sizeof(inquiry_info));
+        int num_rsp = hci_inquiry(bt_adapter_id, SCAN_LENGTH, MAX_DEV_RSP, NULL, &scanned_devices, IREQ_CACHE_FLUSH);
+        Bluetooth_displayAvailableDevices(scanned_devices, num_rsp);
+        
+        sleep(5);
+        free(scanned_devices);
+        scanned_devices = NULL;
 
 
     }
 
     return NULL;
+}
+
+
+
+
+void checkError(void){
+    switch (errno) {
+        case EADDRNOTAVAIL:
+            fprintf(stderr, "Error: Bluetooth hardware address not available\n");
+            exit(1);
+            break;
+        case EFAULT:
+            fprintf(stderr, "Error: Invalid memory location for Bluetooth hardware address\n");
+            exit(1);
+            break;
+        case EINVAL:
+            fprintf(stderr, "Error: Invalid Bluetooth device identifier\n");
+            exit(1);
+            break;
+        case EBADF:
+            fprintf(stderr, "Error: Invalid Bluetooth device id\n");
+            exit(1);
+            break;
+        case EBUSY:
+            fprintf(stderr, "Error: Bluetooth adapter already in use\n");
+            exit(1);
+            break;
+        case EIO:
+            fprintf(stderr, "Error: IO error when attempting to open Bluetooth adapter\n");
+            exit(1);
+            break;
+        case ENODEV:
+            fprintf(stderr, "Error: Bluetooth adapter does not exist or is unavailable\n");
+            exit(1);
+            break;
+        case ENOMEM:
+            fprintf(stderr, "Error: Insufficient memory to allocate Bluetooth resources\n");
+            exit(1);
+            break;
+        case ENOTTY:
+            fprintf(stderr, "Error: Specified fd is not a TTY device\n");
+            exit(1);
+            break;
+        default:
+            // no error
+            break;
+    }
+    
 }
--- source-files/bluetooth.h
+++ source-files/bluetooth.h
@@ -1,10 +1,16 @@
 #ifndef BLUETOOTH_H
 #define BLUETOOTH_H
 
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+
 void Bluetooth_init(void);
 
 void Bluetooth_cleanup(void);
 
+void Bluetooth_displayAvailableDevices(inquiry_info* devices, int num_devices);
 
+void checkError(void);
 
 #endif
